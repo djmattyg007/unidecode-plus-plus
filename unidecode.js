67ecd16b80fd806a48a333212b01fe4965fc241d
Matthew Gamble <git@matthewgamble.net>
2025-10-30 22:47:55 +1100

    Initial code cleanup
    The biggest thing I wanted to fix up was the option handling. It was
    super problematic, because previous invocations could poison future
    invocations. Now there are no global variables, except for the data
    cache. There's also the codepoints regex, but that's actually an
    immutable constant.
    
    This cleanup also features some variable and function name cleanup, a
    lot of syntax cleanup (always use braces), and a small modernisation of
    Javascript code generally.

--- unidecode.js
+++ unidecode.js
@@ -6,7 +6,7 @@
  * as transliterating the Greek letter `ρ` (rho) as the ASCII `p`, even though it sounds more like an `r`. Various
  * emojis are represented either as "ASCII art" or English text.
  *
- * The tables used (in data) are converted from the tables provided in the Perl libraryText::Unidecode
+ * The tables used (in the data folder) are converted from the tables provided in the Perl libraryText::Unidecode
  * (http://search.cpan.org/dist/Text-Unidecode/lib/Text/Unidecode.pm) and are distributed under the Perl license.
  *
  * Whereas the original JavaScript and Perl versions of Unidecode only worked the Unicode Basic Multilingual Plane
@@ -20,97 +20,105 @@
 
 'use strict';
 
-var tr = {};
-var codepoints;
+const dataCache = {};
 
+let codepoints;
 try {
   // Can we use Unicode-aware regexes?
   codepoints = /[^\x00-\x7F]/gu; // jshint ignore:line
-}
-catch (err) {
+} catch (err) {
   // Nope! This mess will have to do.
   codepoints = /(?:[\x80-\uD7FF\uE000-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF])/g;
 }
 
-var german = false;
-var deferredSmartSpacing = false;
-var smartSpacing = false;
-var skipRanges;
+function prepareOptions(options) {
+  return {
+    ...(options.german !== undefined ? { german: options.german } : { german: false }),
+    ...(options.deferredSmartSpacing !== undefined ? { deferredSmartSpacing: options.deferredSmartSpacing } : { deferredSmartSpacing: false }),
+    ...(options.deferredSmartSpacing ? { smartSpacing: true } : (options.smartSpacing !== undefined ? { smartSpacing: options.smartSpacing } : { smartSpacing: false })),
+    ...(options.skipRanges !== undefined ? { skipRanges: options.skipRanges } : { skipRanges: [] }),
+  };
+}
 
-module.exports = function unidecode(str, options) {
-  if (!str)
+module.exports = function unidecode(inputStr, inputOptions) {
+  if (!inputStr) {
     return '';
+  }
 
-  german = options && options.german;
-  deferredSmartSpacing = options && options.deferredSmartSpacing;
-  smartSpacing = deferredSmartSpacing || (options && options.smartSpacing);
-  skipRanges = options && options.skipRanges;
+  const options = prepareOptions(inputOptions ?? {});
 
-  if (german)
+  let str = inputStr;
+
+  if (options.german) {
     str = str.replace(/([AOU])\u0308/g, '$1E').replace(/([aou])\u0308/g, '$1e');
+  }
 
-  str = str.replace(codepoints, unidecode_internal_replace);
+  str = str.replace(codepoints, (match) => {
+    return replacer(match, options);
+  });
 
-  if (!smartSpacing || deferredSmartSpacing)
+  if (!options.smartSpacing || options.deferredSmartSpacing) {
     return str;
-  else
+  } else {
     return resolveSpacing(str);
+  }
 };
 
-function unidecode_internal_replace(ch) {
-  var cp = ch.codePointAt(0), ch0 = ch;
+function replacer(ch, options) {
+  var cp = ch.codePointAt(0);
 
-  if (skipRanges) {
-    for (var i = 0; i < skipRanges.length; ++i) {
-        if (skipRanges[i][0] <= cp && cp <= skipRanges[i][1])
-            return ch;
+  if (options.skipRanges.length > 0) {
+    for (var i = 0; i < options.skipRanges.length; ++i) {
+      if (options.skipRanges[i][0] <= cp && cp <= options.skipRanges[i][1]) {
+        return ch;
+      }
     }
   }
 
   var high = cp >> 8;
-  var row = high + (high === 0 && german ? 0.5 : 0);
+  var row = high + (high === 0 && options.german ? 0.5 : 0);
   var low = cp & 0xFF;
   var emDash = cp === 0x2014;
   // This doesn't cover all emoji, just those currently defined.
   var emoji = (high === 0x1F4 || high === 0x1F6 || high === 0x1F9);
 
-  if (0x18 < high && high < 0x1E || 0xD7 < high && high < 0xF9)
+  if (0x18 < high && high < 0x1E || 0xD7 < high && high < 0xF9) {
     return ''; // Isolated high or low surrogate
-  else if (high > 0xFF && !emoji) {
+  } else if (high > 0xFF && !emoji) {
     return '_';
   }
 
-  if (!tr[row]) {
+  if (!dataCache[row]) {
     try {
-      tr[row] = require('./data/x' + (row < 0x10 ? '0' : '') + row.toString(16));
+      dataCache[row] = require('./data/x' + (row < 0x10 ? '0' : '') + row.toString(16));
 
       // I'm not sure why, but a fair number of the original data tables don't contain a full 256 items.
-      if (tr[row].length < 256) {
-        var start = tr[row].length;
-        tr[row].length = 256;
-        tr[row].fill('_', start);
+      if (dataCache[row].length < 256) {
+        var start = dataCache[row].length;
+        dataCache[row].length = 256;
+        dataCache[row].fill('_', start);
       }
-    }
-    catch (err) {
-      tr[row] = new Array(256).fill('_');
+    } catch (err) {
+      dataCache[row] = new Array(256).fill('_');
     }
   }
 
-  ch = tr[row][low];
+  ch = dataCache[row][low];
 
-  if (smartSpacing && emDash)
+  if (options.smartSpacing && emDash) {
     return '\x80--\x80';
-  if (!smartSpacing || ch === '[?]' || ch === '_' || /^\w+$/.test(ch))
+  } if (!options.smartSpacing || ch === '[?]' || ch === '_' || /^\w+$/.test(ch)) {
     return ch;
-  else if (emoji)
+  } else if (emoji) {
     return '\x80\x81' + ch + '\x81\x80';
-  else
+  } else {
     return '\x80' + ch.trim() + '\x80';
+  }
 }
 
 function resolveSpacing(str) {
   return str
-    .replace(/(\w)(\x80--\x80)(\w)/g, function(_, p1, _2, p3) { return p1 + ' - ' + p3; })
+    .replace(/(\w)(\x80--\x80)(\w)/g, (_, p1, _2, p3) => p1 + ' - ' + p3)
     .replace(/\x80(?!\w)/g, "")
     .replace(/\x80\x80|(\w)\x80/g, "$1\x81")
     .replace(/\x80/g, "")
